080a5d789e4608260cbb4dd6f37e24ad2c8b04b5
pradhanDebkanta <debkantapradhan001@gmail.com>
2022-09-26 18:09:11 +0530

    few changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
+import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import '../assets/css/globals.css';
 import { createTheme, NextUIProvider } from "@nextui-org/react"
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 // import TopNavbar from '../components/navbar';
-const TopNavbar=dynamic(()=>import('../components/navbar'), {ssr: false})
+const TopNavbar = dynamic(() => import('../components/navbar'), { ssr: false })
 
 const lightTheme = createTheme({
   type: 'light',
@@ -21,6 +22,13 @@ const darkTheme = createTheme({
 
 function MyApp({ Component, pageProps }) {
 
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      console.log = () => { };
+      console.warn = () => { };
+    }
+
+  }, [])
   return (
     <NextThemesProvider
       defaultTheme='system'
