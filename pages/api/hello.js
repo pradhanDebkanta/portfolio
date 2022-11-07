427e931eafd4d077862b99ec296e6dbb140755b0
pradhanDebkanta <debkantapradhan001@gmail.com>
2022-11-07 20:21:41 +0530

    ip testing

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -6,13 +6,17 @@ import requestIp from 'request-ip';
 async function handelers(req, res) {
     let clientIp = ip.address();
     let clientIp1 = requestIp.getClientIp(req);
-    console.log(clientIp, 'clientIp');
-    console.log(clientIp1, 'clientIp1');
-    res.send({
-        ip: clientIp,
-        requestIp: clientIp1
-    })
+    // console.log(clientIp, 'clientIp');
+    // console.log(clientIp1, 'clientIp1');
+
+    // res.send({
+    //     ip: clientIp,
+    //     requestIp: clientIp1
+    // })
     // console.log(NextRequest.ip)
+    let checkIP =  req.connection._peername;
+    console.log('ip', checkIP);
+    res.send({ ip: checkIP})
 };
 
 export default handelers;
