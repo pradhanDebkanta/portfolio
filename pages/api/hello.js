11b95dd0b0d624a38a0af43fb25f2c2feb59e8cf
pradhanDebkanta <debkantapradhan001@gmail.com>
2022-11-07 20:39:19 +0530

    ip testing

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -14,9 +14,26 @@ async function handelers(req, res) {
     //     requestIp: clientIp1
     // })
     // console.log(NextRequest.ip)
-    let checkIP =  req.connection._peername;
+    let checkIP = {
+        xcip: req.headers['x-client-ip'] || `req.headers['x-client-ip']`,
+        cfcip: req.headers['cf-connecting-ip'] || `req.headers['cf-connecting-ip']`,
+        fcip: req.headers['fastly-client-ip'] || `req.headers['fastly-client-ip']`,
+        tcip: req.headers['true-client-ip'] || `req.headers['true-client-ip']`,
+        xrip: req.headers['x-real-ip'] || `req.headers['x-real-ip']`,
+        xccip: req.headers['x-cluster-client-ip'] || `req.headers['x-cluster-client-ip']`,
+        xfip: req.headers['x-forwarded'] || `req.headers['x-forwarded']`,
+        xffip: req.headers['forwarded-for'] || `req.headers['forwarded-for']`,
+        fip: req.headers.forwarded || `req.headers.forwarded`,
+        xauip: req.headers['x-appengine-user-ip'] || `req.headers['x-appengine-user-ip']`,
+        rip: req.connection?.remoteAddress || `req.connection.remoteAddress`,
+        raip: req.connection?.socket?.remoteAddress || `req.connection.socket.remoteAddress`,
+        socraip: req.socket?.remoteAddress || `req.socket.remoteAddress`,
+        iraip: req.info?.remoteAddress || `req.info.remoteAddress`,
+        sorceip: req.requestContext?.identity?.sourceIp || `req.requestContext.identity.sourceIp`,
+        cf: req.headers['Cf-Pseudo-IPv4'] || `req.headers['Cf-Pseudo-IPv4']`,
+    };
     console.log('ip', checkIP);
-    res.send({ ip: checkIP})
+    res.send(checkIP)
 };
 
 export default handelers;
